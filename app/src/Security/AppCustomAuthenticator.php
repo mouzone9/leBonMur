96509b4e21fb46ff8627da678666b4004dd8a8f6
Jessy <ernatusjessy@gmail.com>
2022-01-31 21:37:26 +0100

    fix the registration form, don't use the login form he doesn't works

--- app/src/Security/AppCustomAuthenticator.php
+++ app/src/Security/AppCustomAuthenticator.php
@@ -50,7 +50,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate(''));
+        return new RedirectResponse($this->urlGenerator->generate('index'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
